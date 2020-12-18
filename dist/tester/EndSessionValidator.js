1c2a01fa0fedf3bf67a41cdc14c19aeda6b9215b
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-12-18 01:07:44 -0800

    Documentation changes.

--- dist/tester/EndSessionValidator.js
+++ dist/tester/EndSessionValidator.js
@@ -9,12 +9,21 @@ const types_1 = require("../types");
 class EndSessionValidator extends types_1.ResponseValidator {
     validate(currentItem, response) {
         // check the shouldEndSession flag
-        if (currentItem.shouldEndSession === true && response.response.shouldEndSession === false) {
-            assert_1.fail('the response did not end the session');
+        const actualShouldEndSession = response.response.shouldEndSession;
+        if (currentItem.shouldEndSession === actualShouldEndSession) {
+            return;
         }
-        else if (currentItem.shouldEndSession === false && response.response.shouldEndSession !== false) {
-            assert_1.fail('the response ended the session');
+        let failMessage = `shouldEndSession was in an unexpected state. Expected: ${currentItem.shouldEndSession}. Actual: ${actualShouldEndSession}`;
+        if (currentItem.shouldEndSession === true && actualShouldEndSession !== true) {
+            failMessage = 'the response did not end the session';
         }
+        else if (currentItem.shouldEndSession === false && actualShouldEndSession !== false) {
+            failMessage = 'the response ended the session';
+        }
+        else if (currentItem.shouldEndSession === undefined && actualShouldEndSession !== undefined) {
+            failMessage = 'end of session was not left in an undefined state';
+        }
+        assert_1.fail(failMessage);
     }
 }
 exports.EndSessionValidator = EndSessionValidator;
